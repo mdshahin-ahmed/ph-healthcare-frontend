ad98b8bfd4bd53d88964f7df974183382aa52237
shahinahmed6057@gmail.com <mdshahinahmed605701@gmail.com>
2024-05-08 21:23:05 +0600

    Add: footer

--- src/components/Shared/Footer/footer.tsx
+++ src/components/Shared/Footer/footer.tsx
@@ -16,8 +16,39 @@ const Footer = () => {
           <Typography color="#ffffff">Diagnostics</Typography>
           <Typography color="#ffffff">NGOs</Typography>
         </Stack>
-        <Stack direction="row" justifyContent="center" gap={4}>
-          <Image src={facebookIcon} width={40} height={40} alt="Facebook" />
+        <Stack direction="row" justifyContent="center" py={3} gap={2}>
+          <Image src={facebookIcon} width={30} height={30} alt="Facebook" />
+          <Image src={facebookIcon} width={30} height={30} alt="Facebook" />
+          <Image src={facebookIcon} width={30} height={30} alt="Facebook" />
+          <Image src={facebookIcon} width={30} height={30} alt="Facebook" />
+        </Stack>
+        <div className="border-b-[1px] border-dashed"></div>
+        <Stack
+          alignItems="center"
+          direction="row"
+          justifyContent="space-between"
+          py={3}
+          gap={2}
+        >
+          <Typography component="p" color="white">
+            &copy; 2024 PH HealthCare. All Rights Reserved.
+          </Typography>
+          <Typography
+            color="white"
+            component={Link}
+            href="/"
+            variant="h4"
+            fontWeight={600}
+          >
+            P
+            <Box component="span" color="primary.main">
+              H
+            </Box>{" "}
+            Health Care
+          </Typography>
+          <Typography component="p" color="white">
+            Privacy Policy! Teams & Conditions
+          </Typography>
         </Stack>
       </Container>
     </Box>
