8e191649d3e107170d2e07b76f1bb9cbc3801412
shahinahmed6057@gmail.com <mdshahinahmed605701@gmail.com>
2024-06-09 12:17:47 +0600

    48.6

--- src/helpers/axios/axiosInstance.ts
+++ src/helpers/axios/axiosInstance.ts
@@ -37,7 +37,7 @@ instance.interceptors.response.use(
     return responseObject;
   },
   function (error) {
-    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    // Any status codes that falls outside the range of 2xx cause this function to trigger.
     // Do something with response error
     const responseObject: IGenericErrorResponse = {
       statusCode: error?.response?.data?.statusCode || 500,
